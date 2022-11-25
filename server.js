df968cd2883b9858e4d9f2ff191c22dafbce6afd
DanielMalede <danielmalede@gmail.com>
2022-11-25 15:29:47 +0200

    /

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const server  = require('http').createServer()
+const fs = require("fs");
+const server = require("http").createServer((req, res) => {
+  fs.readFile("./index.html", (error, result) => res.end(result));
+});
+const io = require("socket.io")(server);
 
-server.on('request',(req,res)=>res.end('server online'))
 
-server.listen(5000)
+io.on("connection", (socket) => {
+  socket.on("message", (textmsg) => {
+    io.emit("message", textmsg);
+  });
+});
+
+
+server.listen(5000);
